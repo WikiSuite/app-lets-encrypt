5b98401b56d9915369da58110710596321ccc19e
Polyna-Maude R.-Summerside <devel@polynamaude.com>
2018-10-14 19:54:50 -0400

    Translated strings to french canadian

---
+++ language/fr_CA/lets_encrypt_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['lets_encrypt_app_name'] = 'Let\'s Encrypt';
+$lang['lets_encrypt_app_description'] = 'Let\'s Encrypt est une autorité de certication qui fournit des cerificats X.509 (SSL) gratuits.';
+$lang['lets_encrypt_certificate'] = 'Certificat';
+$lang['lets_encrypt_certificates'] = 'Certificats';
+$lang['lets_encrypt_expires'] = 'Expirés';
+$lang['lets_encrypt_default_email_address'] = 'Adresse courriel par défaut';
+$lang['lets_encrypt_issued'] = 'Émis';
+$lang['lets_encrypt_domains'] = 'Domaines';
+$lang['lets_encrypt_primary_domain'] = 'Domaine principal';
+$lang['lets_encrypt_other_domains'] = 'Autre domaines';
+$lang['lets_encrypt_domain_list_invalid'] = 'La liste de domaines est invalide.';
+$lang['lets_encrypt_certificate_created'] = 'Certificat crée';
+$lang['lets_encrypt_certificate_created_help'] = 'Votre certificat SSL a été crée!  Détails ci-bas.';
+$lang['lets_encrypt_renew_failed'] = 'La validation du renouvellement de cerificat a échoué.';
+$lang['lets_encrypt_renew_succeeded'] = 'La validation du renouvellement de certificat a réussie.';
+$lang['lets_encrypt_renew_not_required'] = 'Renouvellement de certificat non requis - aucun renouvellement prévu.';
+$lang['lets_encrypt_connection_refused_warning'] = 'Le système Let\'s Encrypt n\`a pas réussis à se connecter pour la validation.  SVP consultez la documentation pour plus d\'information !  Les détails de l\'échec ci-bas.';
